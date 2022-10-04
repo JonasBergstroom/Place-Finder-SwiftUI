02cee472a5cfa9643a256152cd6bf3fee71f621c
jonasbergstrom <jontrian97@gmail.com>
2022-10-04 17:50:56 +0200

    Initial Commit

---
+++ Place-Finder-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Place-Finder-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Place-Finder-SwiftUI/Place_Finder_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Place_Finder_SwiftUIApp.swift
+//  Place-Finder-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-04.
+//
+
+import SwiftUI
+
+@main
+struct Place_Finder_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
