5cbe8c0af436a241473d6d99138674f4e217f1ba
jonasbergstrom <jontrian97@gmail.com>
2022-10-04 18:20:14 +0200

    Created the "MapAPI" class with default coordinates values

--- Place-Finder-SwiftUI/MapModel.swift
+++ Place-Finder-SwiftUI/MapModel.swift
@@ -23,3 +23,18 @@ struct Location: Identifiable {
     let name: String
     let coordinate: CLLocationCoordinate2D
 }
+
+class MapAPI: ObservableObject {
+    private let BASE_URL = "http://api.positionstack.com/v1/forward"
+    private let API_KEY = "22c87b81ed3d8a6d84baef912c71e45f"
+    
+    @Published var region: MKCoordinateRegion
+    @Published var coordinates = []
+    @Published var locations: [Location] = []
+    
+    init() {
+        self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 59.334591, longitude: 18.063240), span: MKCoordinateSpan(latitudeDelta: 3, longitudeDelta: 3))
+        
+        self.locations.insert(Location(name: "Pin", coordinate: CLLocationCoordinate2D(latitude: 59.334591, longitude: 18.063240)), at: 0)
+    }
+}
