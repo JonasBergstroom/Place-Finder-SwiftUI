982143cf42d42fe72478e05cd86874bd4912f794
jonasbergstrom <jontrian97@gmail.com>
2022-10-04 18:32:41 +0200

    Created the "getLocation" function for the API request

--- Place-Finder-SwiftUI/MapModel.swift
+++ Place-Finder-SwiftUI/MapModel.swift
@@ -37,4 +37,45 @@ class MapAPI: ObservableObject {
         
         self.locations.insert(Location(name: "Pin", coordinate: CLLocationCoordinate2D(latitude: 59.334591, longitude: 18.063240)), at: 0)
     }
+    
+    func getLocation(address: String, delta: Double) {
+        let pAddress = address.replacingOccurrences(of: " ", with: "%20")
+        let url_string = "\(BASE_URL)?access_key=\(API_KEY)&query=\(pAddress)"
+        
+        guard let url = URL(string: url_string) else {
+            print("Wrong URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let newCoordinates = try? JSONDecoder().decode(Address.self, from: data) else {
+                return
+            }
+            if newCoordinates.data.isEmpty {
+                print("Could not found the address!")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let details = newCoordinates.data[0]
+                let lat = details.latitude
+                let lon = details.longitude
+                let name = details.name
+                
+                self.coordinates = [lat, lon]
+                self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta))
+                
+                let new_location = Location(name: name ?? "Pin", coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon))
+                self.locations.removeAll()
+                self.locations.insert(new_location, at: 0)
+                
+                print("Loaded the location without any problem!")
+            }
+        }
+        .resume()
+    }
 }
